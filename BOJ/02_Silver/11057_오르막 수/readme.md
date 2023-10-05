a01b6728c3e3d39dbab8c3d155c66d032115a573
SeungHwan Kim <tmdghks12@korea.ac.kr>
2023-10-05 21:47:13 +0900

    [BOJ 11057] 오르막 수

---
+++ BOJ/02_Silver/11057_오르막 수/readme.md
@@ -0,0 +1,11 @@
+# [SILVER I] BOJ 11057 - 오르막 수
+
+[문제 링크](https://boj.kr/2747)
+
+### 분류
+
+수학 (Math), 구현 (Implementation), 동적 프로그래밍 (Dynamic Programming)
+
+### 풀이
+
+끝 자리에 따른 점화식으로 구할 수 있다. 여백이 좁아 생략.
---
+++ BOJ/02_Silver/11057_오르막 수/오르막 수.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    vector<vector<long long>> v(1001, vector<long long>(10, 0));
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < 10; i++) {
+        v[1][i] = 1;
+    }
+
+    for (int i = 2; i <= n; i++) {
+        for (int j = 0; j <= 9; j++) {
+            for (int k = 0; k <= j; k++) {
+                v[i][j] += v[i - 1][k];
+            }
+            v[i][j] %= 10007;
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < 10; i++) {
+        sum += v[n][i];
+    }
+
+    cout << sum % 10007 << '\n';
+
+    
+}
