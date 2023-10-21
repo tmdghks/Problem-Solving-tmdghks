3e6d2e1b2993bdb566548b39e235630c8b783fa6
SeungHwan Kim <tmdghks12@korea.ac.kr>
2023-10-21 21:45:56 +0900

    [BOJ 18044] 쉬운 계단수

--- BOJ/02_Silver/10844_쉬운 계단수/쉬운 계단수.cpp
+++ BOJ/02_Silver/10844_쉬운 계단수/쉬운 계단수.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
 int main() {
     ios::sync_with_stdio(false);
@@ -9,5 +10,25 @@ int main() {
     int n;
     cin >> n;
 
+    vector<vector<ll>> v(n + 1, vector<ll>(10, 0));
+    
+    for (int i = 1; i <= 9; i++) {
+        v[1][i] = 1;
+    }
+
+    for (int i = 2; i <= n; i++) {
+        v[i][0] = v[i - 1][1];
+        v[i][9] = v[i - 1][8];
+        for (int j = 1; j <= 9; j++) {
+            v[i][j] = (v[i-1][j-1] + v[i-1][j+1]) % 1000000000;
+        }
+    }       
+
+    ll sum = 0;
+
+    for (int i = 0; i < 10; i++) {
+        sum += v[n][i];
+    }
+    cout << sum % 1000000000 << '\n';
     
 }
