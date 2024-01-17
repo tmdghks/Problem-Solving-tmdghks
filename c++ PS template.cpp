54510c23da9b0dd26fd3408ca8aed5b1971be83c
SeungHwan Kim <tmdghks12@korea.ac.kr>
2024-01-17 14:29:54 +0900

    [20240117] uploaded c++ PS template.cpp

---
+++ c++ PS template.cpp
@@ -0,0 +1,27 @@
+// 11049 행렬 곱셈 순서
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using LL = unsigned long long;
+using lll = __int128_t;
+using LLL = __uint128_t; // 128비트 정수는 cout, printf 등으로 출력할 수 없음에 유의
+
+// 3-pair
+struct dt {
+    ll a;
+    ll b;
+};
+
+// vector<pair>
+using vdt = vector<dt>;
+
+ll ans;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+}
