a9fb370af7779a73c6344b85bd7f4f7bed73a143
tmdghks <tmdghks12@korea.ac.kr>
2024-06-29 23:11:38 +0900

    [BOJ 17946] 피자는 나눌 수록 커지잖아요

---
+++ BOJ/01_Bronze/17296_피자는 나눌 수록 커지잖아요/피자는 나눌 수록 커지잖아요.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+struct dt {
+    ll x;
+    ll y;
+    ll depth;
+};
+
+ll dx[4] = {0, 0, 1, -1};
+ll dy[4] = {1, -1, 0, 0};
+
+int main() {
+    ll N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        ll tmp;
+        cin >> tmp;
+        cout << 1 << "\n";
+    }
+}
